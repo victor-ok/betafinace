a812945a47b5f3897c8bd0dc2220ab3dffdafc88
victor-ok <vua.okafor@gmail.com>
2023-02-21 09:22:43 +0100

    fixing css

--- betafinance/resources/views/admin-login.blade.php
+++ betafinance/resources/views/admin-login.blade.php
@@ -96,14 +96,14 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="{{ secure_secure_asset('vendor/jquery/jquery.min.js') }}"></script>
-    <script src="{{ secure_secure_asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="{{ secure_secure_asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+    <script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="{{ secure_secure_asset('js/sb-admin-2.min.j') }}"></script>
+    <script src="{{ asset('js/sb-admin-2.min.j') }}"></script>
 
 </body>
 
